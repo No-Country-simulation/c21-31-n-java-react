d96807d93306a02a17da65748626562833c4866b
EzeeRL <ezequielramoslencina@gmail.com>
2024-10-19 22:36:10 -0300

    logo svg agregado en el nav

--- tech-media/components/navInicio.jsx
+++ tech-media/components/navInicio.jsx
@@ -5,13 +5,13 @@ import { useEffect } from "react";
 
 export default function NavInicio({ handleNavClick, paginaActiva }) {
   return (
-    <nav className="bg-gray-800 p-4 rounded-md text-white mb-5">
-      <ul className="flex justify-around">
+    <nav className="bg-gradient-to-r from-blue-600 to-indigo-600 p-4 rounded-md font-bold text-white mb-5">
+      <ul className="flex justify-around text-[18px]">
         <li>
           <button
             onClick={() => handleNavClick("dest")}
-            className={`hover:text-blue-400 ${
-              paginaActiva === "dest" && "text-blue-400"
+            className={`hover:text-yellow-400 transition duration-300 ${
+              paginaActiva === "dest" && "text-yellow-400"
             }`}
           >
             Feed
@@ -20,8 +20,8 @@ export default function NavInicio({ handleNavClick, paginaActiva }) {
         <li>
           <button
             onClick={() => handleNavClick("proyectos")}
-            className={`hover:text-blue-400 ${
-              paginaActiva === "proyectos" && "text-blue-400"
+            className={`hover:text-yellow-400 transition duration-300 ${
+              paginaActiva === "proyectos" && "text-yellow-400"
             }`}
           >
             Proyectos
@@ -30,8 +30,8 @@ export default function NavInicio({ handleNavClick, paginaActiva }) {
         <li>
           <button
             onClick={() => handleNavClick("destacados")}
-            className={`hover:text-blue-400 ${
-              paginaActiva === "destacados" && "text-blue-400"
+            className={`hover:text-yellow-400 transition duration-300 ${
+              paginaActiva === "destacados" && "text-yellow-400"
             }`}
           >
             Destacados
